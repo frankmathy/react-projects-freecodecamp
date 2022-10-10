f1231b7bb56a05153d0a35cccc049c20daaa3b32
Frank Mathy <frankmathy@gmail.com>
2022-10-10 22:35:32 +0200

    Finished project

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section className="title">
+      <h1>our tours</h1>
+      <div className="underline"></div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
